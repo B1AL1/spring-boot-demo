e6e72024c6ed52ccb969a68c0be7021b3837e303
B1AL1 <k.konrad9902@gmail.com>
2022-04-10 00:41:05 +0200

    added transient for age and post mapping

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,36 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student konrad = new Student(
+                    1L,
+                    "Konrad",
+                    "konrad.kalman@pollub.edu.pl",
+                    LocalDate.of(1999, Month.FEBRUARY, 19)
+            );
+
+            Student kacper = new Student(
+                    2L,
+                    "Kacper",
+                    "kaper.kasinski@pollub.edu.pl",
+                    LocalDate.of(2000, Month.MARCH, 16)
+            );
+
+            repository.saveAll(
+                    List.of(konrad, kacper)
+            );
+        };
+    }
+}
